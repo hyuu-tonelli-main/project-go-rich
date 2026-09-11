986adb78064ba23780fdd936b47f330b26c98ffb
Suganzie <agusprib1234@gmail.com>
2026-09-11 10:16:59 +0700

    Create main.go

---
+++ bot/main.go
@@ -0,0 +1,47 @@
+// Binary bg-run implements alternative to Run pattern.
+package main
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+
+	"github.com/gotd/log/logzap"
+
+	"github.com/gotd/contrib/bg"
+
+	"github.com/iamxvbaba/td/examples"
+	"github.com/iamxvbaba/td/telegram"
+)
+
+func main() {
+	// Some users find explicit client.Run(ctx, f) pattern not very convenient.
+	//
+	// However, it is possible to implement wrapper and use classic "Connect"
+	// pattern instead.
+	//
+	// The `contrib/bg` package is example implementation of such pattern.
+	examples.Run(func(ctx context.Context, log *zap.Logger) error {
+		client, err := telegram.ClientFromEnvironment(telegram.Options{
+			Logger: logzap.New(log),
+		})
+		if err != nil {
+			return err
+		}
+
+		// bg.Connect will call Run in background.
+		// Call stop() to disconnect and release resources.
+		stop, err := bg.Connect(client)
+		if err != nil {
+			return err
+		}
+		defer func() { _ = stop() }()
+
+		// Now you can use client.
+		if _, err := client.Auth().Status(ctx); err != nil {
+			return err
+		}
+
+		return nil
+	})
+}
